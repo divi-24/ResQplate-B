f08a947a09386353ddafbdc42bb2b46cdf135042
_VibhorSharma_ <77531894+VibhorSharma199@users.noreply.github.com>
2025-02-22 18:13:02 +0530

    Fixed db

--- app.js
+++ app.js
@@ -1,44 +1,39 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import cors from 'cors';
-import dotenv from 'dotenv';
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
 import dbConnect from "./config/db.js";
-
+export const MONGO_URL = process.env.MONGO_URL;
 app.use(express.json());
 app.use(cors());
-
+console.log(process.env.MONGO_URL);
 dbConnect();
 
 // Routes
-import authRoutes from './routes/authRoutes.js';
-import deliveryRoutes from './routes/deliveryRoutes.js';
+import authRoutes from "./routes/authRoutes.js";
+import deliveryRoutes from "./routes/deliveryRoutes.js";
 import foodDonationRoutes from "./routes/foodDonationRoutes.js";
 import ngoRoutes from "./routes/ngoRoutes.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
 import pickupRoutes from "./routes/pickupRoutes.js";
 
-import sendMail from './routes/Mail Routes/mailRoutes.js';
+import sendMail from "./routes/Mail Routes/mailRoutes.js";
 
-
-
-
-app.use('/api/v1/mail', sendMail);
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/delivery', deliveryRoutes);
-app.use('/api/v1/fooddonation', foodDonationRoutes);
-app.use('/api/v1/ngo', ngoRoutes);
-app.use('/api/v1/notification', notificationRoutes);
-app.use('/api/v1/pickup', pickupRoutes);
-
+app.use("/api/v1/mail", sendMail);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/delivery", deliveryRoutes);
+app.use("/api/v1/fooddonation", foodDonationRoutes);
+app.use("/api/v1/ngo", ngoRoutes);
+app.use("/api/v1/notification", notificationRoutes);
+app.use("/api/v1/pickup", pickupRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Server is ready");
+  res.send("Server is ready");
 });
-
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- config/db.js
+++ config/db.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import { MONGO_URL } from "../app.js";
 
+const dbConnect = async () => {
+  try {
+    const dbConnect = await mongoose.connect(MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    // console.log(MONGO_URL);
+    process.exit(1);
+  }
+};
 
-const dbConnect = async ()=>{
-    try {
-        const dbConnect = await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error.message);
-        process.exit(1);
-    }
-}
-
-
-export default dbConnect 
+export default dbConnect;
